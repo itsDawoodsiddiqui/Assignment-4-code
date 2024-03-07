9b5671c517e825941aa40b498f20f6c70b196ba0
Dawood Siddiqui <siddiquidawood008@gmail.com>
2024-03-07 15:17:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+alert("THIS IS MY FOURTH CLASS")
+console.log("Welcome to my first js work")
+
+var my_name = "Dawood Siddiqui";
+console.log(my_name)
+
+
+var my_age = 14;
+var my_age = 15;
+console.log(my_age);
+
+var first_name = "Dawood";
+var last_name = "Siddiqui";
+var awake =  "true";
+var details = "first_name" + " " + "last_name" + " " + "awake";  
+console.log(details);
+
+var city = "Lahore"
+var address = "I live in" + " " +  city;
+console.log(address);
+
+var mobile_price = 50000;
+var last_price = 1000;
+console.log(mobile_price + 2000 + last_price )
